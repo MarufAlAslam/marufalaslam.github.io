ecf4b070b4b1ca586df298532ddb2518d1e365ea
maruf <marufalaslam@gmail.com>
2020-11-02 02:05:02 +0600

    expertise section added

---
+++ script.js
@@ -0,0 +1,40 @@
+$('#html').LineProgressbar({
+    percentage: 95,
+    fillBackgroundColor: '#03c229',
+    backgroundColor: '#EEEEEE',
+    radius: '0px',
+    height: '10px',
+    width: '100%'
+  });
+
+  $('#css').LineProgressbar({
+    percentage: 100,
+    fillBackgroundColor: '#03c229',
+    backgroundColor: '#EEEEEE',
+    radius: '0px',
+    height: '10px',
+    width: '100%'
+  });
+
+  $('#js').LineProgressbar({
+    percentage: 70,
+    fillBackgroundColor: '#03c229',
+    backgroundColor: '#EEEEEE',
+    radius: '0px',
+    height: '10px',
+    width: '100%'
+  });
+
+  $('#php').LineProgressbar({
+    percentage: 80,
+    fillBackgroundColor: '#03c229',
+    backgroundColor: '#EEEEEE',
+    radius: '0px',
+    height: '10px',
+    width: '100%'
+  });
+  
+  
+//   $("#expmenu").click(function(){
+//     $("#expertise").scrollTo($(window),1000,y);
+//   });
