cdf1662b75c8f250cb99f743e125fe3176df976d
maruf <marufalaslam@gmail.com>
2020-11-05 08:02:25 +0600

    something added

--- script.js
+++ script.js
@@ -1,40 +1,46 @@
-$('#html').LineProgressbar({
-    percentage: 95,
-    fillBackgroundColor: '#03c229',
-    backgroundColor: '#EEEEEE',
-    radius: '0px',
-    height: '10px',
-    width: '100%'
-  });
+$(document).ready(function(){
 
-  $('#css').LineProgressbar({
-    percentage: 100,
-    fillBackgroundColor: '#03c229',
-    backgroundColor: '#EEEEEE',
-    radius: '0px',
-    height: '10px',
-    width: '100%'
-  });
+    $('#navLink').click(function(e){
+        var linkOf = $(this).attr('href');
+        console.log(linkOf);
+    });
 
-  $('#js').LineProgressbar({
-    percentage: 70,
-    fillBackgroundColor: '#03c229',
-    backgroundColor: '#EEEEEE',
-    radius: '0px',
-    height: '10px',
-    width: '100%'
-  });
-
-  $('#php').LineProgressbar({
-    percentage: 80,
-    fillBackgroundColor: '#03c229',
-    backgroundColor: '#EEEEEE',
-    radius: '0px',
-    height: '10px',
-    width: '100%'
-  });
-  
-  
-//   $("#expmenu").click(function(){
-//     $("#expertise").scrollTo($(window),1000,y);
-//   });
+    $('#html').LineProgressbar({
+        percentage: 95,
+        fillBackgroundColor: '#03c229',
+        backgroundColor: '#EEEEEE',
+        radius: '0px',
+        height: '10px',
+        width: '100%'
+      });
+    
+      $('#css').LineProgressbar({
+        percentage: 100,
+        fillBackgroundColor: '#03c229',
+        backgroundColor: '#EEEEEE',
+        radius: '0px',
+        height: '10px',
+        width: '100%'
+      });
+    
+      $('#js').LineProgressbar({
+        percentage: 70,
+        fillBackgroundColor: '#03c229',
+        backgroundColor: '#EEEEEE',
+        radius: '0px',
+        height: '10px',
+        width: '100%'
+      });
+    
+      $('#php').LineProgressbar({
+        percentage: 80,
+        fillBackgroundColor: '#03c229',
+        backgroundColor: '#EEEEEE',
+        radius: '0px',
+        height: '10px',
+        width: '100%'
+      });
+      
+      
+    
+});
